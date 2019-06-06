7071a11b71682b94ebc90fd094af1d0575eb4568
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2019-06-06 01:09:02 -0300

    adding project templates

--- src/cli.ts
+++ src/cli.ts
@@ -1,9 +1,9 @@
-const { build } = require('gluegun')
+import { build } from 'gluegun'
 
 /**
  * Create the cli and kick it off
  */
-async function run(argv) {
+async function run (argv) {
   // create a CLI runtime
   const cli = build()
     .brand('restify-scaffold')
